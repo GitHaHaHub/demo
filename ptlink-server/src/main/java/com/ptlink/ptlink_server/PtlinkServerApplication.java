de3c7c25bdcf5d9a9516a98e4078dcc096233e6c
haha <swidhaha@outlook.com>
2024-11-06 03:06:09 +0900

    xd
    ddd

---
+++ ptlink-server/src/main/java/com/ptlink/ptlink_server/PtlinkServerApplication.java
@@ -0,0 +1,13 @@
+package com.ptlink.ptlink_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtlinkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtlinkServerApplication.class, args);
+	}
+
+}
